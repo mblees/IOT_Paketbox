39dcbcdc89b7c50525836fdc5168321901d2c451
mblees <104490472+mblees@users.noreply.github.com>
2024-02-05 15:15:42 +0100

    initial commit

---
+++ Python Service/main.py
@@ -0,0 +1,41 @@
+from homeassistant_api import Client
+import paho.mqtt.client as mqtt
+
+broker_address = "eu1.cloud.thethings.network"
+broker_port = 1883
+username = "paketbox-iot@ttn"
+passkey = "NNSXS.VBOIQGBJTR2OQYPPH2PZP2WIOQWY532HTYXS2EY.BIUKFEZFWS2L56VCH4JBWJQBLMWUH3OJPCSLWKPFJJBHUTRHYAIQ"
+topic = "v3/paketbox-iot@ttn/devices/eui-70b3d57ed0064b51/up"
+
+
+URL = 'http://192.168.0.164:8123/api'
+TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' \
+        '.eyJpc3MiOiIzMjFiMDk2YWMzYmE0OWQ2OTkxODA2ZTFmYmQ2NTQzNyIsImlhdCI6MTcwNzEzODI1NiwiZXhwIjoyMDIyNDk4MjU2fQ' \
+        '.piCuYWiscPjG88Fj3AQrX3suJqP-o4oKEQcQpxeSl_E'
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT broker")
+        client.subscribe(topic)
+    else:
+        print(f"Connection failed with code {rc}")
+
+
+def on_message(client, userdata, msg):
+    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
+
+    home_assistant_client = Client(URL, TOKEN)
+    home_assistant_client.trigger_service('notify', 'notify', message='Es ist ein Paket in der Paketbox angekommen!', title='Alarm')
+
+
+mqtt = mqtt.Client()
+
+mqtt.on_connect = on_connect
+mqtt.on_message = on_message
+
+mqtt.username_pw_set(username=username, password=passkey)
+
+mqtt.connect(broker_address, broker_port, keepalive=60)
+
+mqtt.loop_forever()
